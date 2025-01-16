fba1e3aece61415b97538c0f3961694ae58533aa
lh2513 <114601271+lh2513@users.noreply.github.com>
2025-01-16 08:41:38 +0800

    Create configdx.py

---
+++ configdx.py
@@ -0,0 +1,18 @@
+ip_version_priority = "ipv6"
+
+source_urls = [
+    "https://raw.githubusercontent.com/ngdikman/hksar/refs/heads/main/guangdian.m3u"
+]
+
+url_blacklist = [
+    "/4403-tx."
+]
+
+epg_urls = [
+    "https://epg.v1.mk/pp.xml",
+    "https://live.fanmingming.com/e.xml",
+    "http://epg.51zmt.top:8000/e.xml",
+    "https://epg.pw/xmltv/epg_CN.xml",
+    "https://epg.pw/xmltv/epg_HK.xml",
+    "https://epg.pw/xmltv/epg_TW.xml"
+]
